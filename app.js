dd48d60ab8217c651dde9d58ddd7d0b46d98ebda
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-24 11:03:51 -0500

    paso 2-for

--- app.js
+++ app.js
@@ -6,20 +6,23 @@
 const turnos = [
     {codigo:"S-005", nombre:"Turalyon", tramite:"Solicitud de cita médica general", modulo:"A15", atendido:true},
     {codigo:"S-225", nombre:"Alleria", tramite:"Reclamo de medicamentos", modulo:"B05", atendido:false},
-    {codigo:"S-184", nombre:"Sylvanas", tramite:"Autorización de cirugías", modulo:"C10", atendido:true},
+    {codigo:"S-184", nombre:"Sylvanas", tramite:"Autorización de cirugías", modulo:"C10", atendido:false},
     {codigo:"S-092", nombre:"Veressa", tramite:"Gestión de servicios de urgencias", modulo:"D02", atendido:true},
     {codigo:"S-103", nombre:"Arathor", tramite:"Inclusión de beneficiarios", modulo:"E09", atendido:true},
-    {codigo:"S-0124", nombre:"Nathanos", tramite:"Solicitud de historia clínica", modulo:"F13", atendido:true},
+    {codigo:"S-0124", nombre:"Nathanos", tramite:"Solicitud de historia clínica", modulo:"F13", atendido:false},
 ];
 
 const visornumero=document.getElementById("visornumero");
 const visorModulo=document.getElementById("visorModulo");
 const buscador=document.getElementById("buscador");
 const contadorFila=document.getElementById("contadorFila");
-const listaEspera=document.getElementById("listaEspera");
 const btnLlamar=document.getElementById("btnLlamar");
 const mensajeVacio=document.getElementById("mensajeVacio");
 
-function pintarfila(params) {
-    
+function pintarfila() {
+    const listaEspera=document.getElementById("listaEspera");
+    for (let i = 0; i < turnos.length; i++) {
+        const nuevoItem = document.createElement("li");
+        
+    }
 }
