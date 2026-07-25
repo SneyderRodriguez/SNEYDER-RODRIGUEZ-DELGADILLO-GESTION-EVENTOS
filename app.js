dd32df6aca311073c1441a732fb8a7c7e767d223
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-24 23:10:39 -0500

    funcion actualizarContador

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-/**Si se va la luz y la pantalla se apaga
- * ¿La persona del turno A-014 perdió su turno?
- * NO
- */
-/** codigo, nombre, tramite, modulo, atendido */
+    /*==================================================================================================
+            ¡PARA PENSAR!
+    * ==================================================================================================
+    *Si se va la luz y la pantalla se apaga
+     * ¿La persona del turno A-014 perdió su turno?
+     * NO
+    * ==================================================================================================*/
 const turnos = [
     { codigo: "S-005", nombre: "Turalyon", tramite: "Solicitud de cita médica general", modulo: "A15", atendido: true },
     { codigo: "S-225", nombre: "Alleria", tramite: "Reclamo de medicamentos", modulo: "B05", atendido: false },
@@ -12,6 +14,15 @@ const turnos = [
     { codigo: "S-0124", nombre: "Nathanos", tramite: "Solicitud de historia clínica", modulo: "F13", atendido: false },/**
     el codigo de 4 digitos es intencional */
 ];
+    /*==================================================================================================
+            ¡PARA PENSAR!
+    * ==================================================================================================
+    *Acabas de escribir seis turnos y guardaste el archivo. Mira la pantalla
+    * ¿Qué ves en la fila de espera? Nada, y ya estaba pensando que hice mal
+    * Si no ves nada, ¿El arreglo existe o no existe? cuando leí esta pregunta: pues si existe
+    * ¿Qué le falta a tu programa para que lo que existe en memoria aparezca en pantalla?
+    * Aquí entendi que hacia falta conectarla
+    * ==================================================================================================*/
 const visornumero = document.getElementById("visornumero");
 const visorModulo = document.getElementById("visorModulo");
 const buscador = document.getElementById("buscador");
@@ -20,7 +31,7 @@ const listaEspera = document.getElementById("listaEspera");
 const mensajeVacio = document.getElementById("mensajeVacio");
 const btnLlamar = document.getElementById("btnLlamar");
 
-function pintarfila() {
+function pintarFila() {
     listaEspera.innerHTML = ""; /**innerHTML > elimina el contenido de ese "elemento" */
     for (let i = 0; i < turnos.length; i++) {
         const nuevoItem = document.createElement("li"); /**createElement("li") = <li> en HTML */
@@ -44,18 +55,25 @@ function pintarfila() {
 
         nuevoSpan.textContent = turnos[i].codigo;
         nuevoP1.textContent = turnos[i].nombre;
-        nuevoP2.textContent = turnos[i].tramite + "-" + turnos[i].modulo;
+        nuevoP2.textContent = turnos[i].tramite + "  -  " + turnos[i].modulo;
         if (turnos[i].atendido) { /**Comparación booleana */
             otroSpan.textContent = "Atendido";
-            nuevoItem.classList.add("turno--atendido");
+            nuevoItem.classList.add("turno--atendido"); /**PUNTO 3 */
         } else {
             otroSpan.textContent = "En espera";
         }
         listaEspera.appendChild(nuevoItem);
     }
+actualizarContador();/**PUNTO 4 */
 }
-pintarfila();
-
+pintarFila();
+    /*==================================================================================================
+            ¡PARA PENSAR!
+    * ==================================================================================================
+    *createElement crea un elemento, pero no aparece en pantalla hasta que usas appendChild.
+    *¿Dónde está el elemento en el momento entre las dos instrucciones? almacenada en la variable
+    *
+    * ==================================================================================================*/
 function llamarSiguiente() {
     let haymasturnos = false; /** esta variable pregunta si hay mas turnos pendientes */
     for (let i = 0; i < turnos.length; i++) {
@@ -63,15 +81,38 @@ function llamarSiguiente() {
             turnos[i].atendido = true; /**aqui verificamos que los turnos pendientes cambien de estado */
             visornumero.textContent = turnos[i].codigo;
             visorModulo.textContent = turnos[i].modulo;
-            pintarfila();
+            pintarFila();
             haymasturnos = true; /**aqui confirmamos que habian mas turnos pendientes */
             break;
         }
     }
     if (!haymasturnos) { /**aqui decimos que como ya se comprobo que todos los turnos estan atendidos */
-        visornumero.textContent = "— — —";/**entonces reemplace el visor de numero con el nuevo texto */
+        visornumero.textContent = "— — —";
+        visorModulo.textContent = "Esperando llamado"/**entonces reemplace el visor de numero con el nuevo texto */
     }
 }
 btnLlamar.addEventListener("click", llamarSiguiente/*function(){
     llamarSiguiente();
 }*/);
+    /*==================================================================================================
+            ¡PARA PENSAR!
+    * ==================================================================================================
+    *Podrías haber cambiado el color de fondo directamente desde JS
+    * ¿Por qué es mejor agregar una clase? porqué así no tendria que pintar "linea por linea" desde js
+    * Si mañana el cliente pide que los turnos atendidos se vean azules en lugar de grises
+    * ¿Qué archivo tendrias que abrir con cada una de las dos formas?
+    * CSS
+    * ==================================================================================================*/
+function actualizarContador() {
+    const sigueespera=document.querySelectorAll(".turno:not(.turno--atendido)");
+    contadorFila.textContent = sigueespera.length;
+}
+    /*==================================================================================================
+            ¡PARA PENSAR!
+    * ==================================================================================================
+    * También podrias haber contado recorriendo el arreglo con un for
+    * ¿Qué diferencia hay entre contar en el arreglo y contar en la pantalla?
+    * RTA: Que el arreglo me contaria todos los elementos que hay dentro?
+    * Imagina que el contador dice 5 pero en pantalla se ven 3 turnos en espera
+    * ¿Cúal de los dos está equivocado, y que habría pasado en tu programa? El contador
+    * ==================================================================================================*/
