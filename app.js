87e4f8f1ce490d9d7e9ce2e157fbe421840cb3df
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-24 19:02:25 -0500

    integrado class

--- app.js
+++ app.js
@@ -4,26 +4,43 @@
  */
 /** codigo, nombre, tramite, modulo, atendido */
 const turnos = [
-    {codigo:"S-005", nombre:"Turalyon", tramite:"Solicitud de cita médica general", modulo:"A15", atendido:true},
-    {codigo:"S-225", nombre:"Alleria", tramite:"Reclamo de medicamentos", modulo:"B05", atendido:false},
-    {codigo:"S-184", nombre:"Sylvanas", tramite:"Autorización de cirugías", modulo:"C10", atendido:false},
-    {codigo:"S-092", nombre:"Veressa", tramite:"Gestión de servicios de urgencias", modulo:"D02", atendido:true},
-    {codigo:"S-103", nombre:"Arathor", tramite:"Inclusión de beneficiarios", modulo:"E09", atendido:true},
-    {codigo:"S-0124", nombre:"Nathanos", tramite:"Solicitud de historia clínica", modulo:"F13", atendido:false},
+    { codigo: "S-005", nombre: "Turalyon", tramite: "Solicitud de cita médica general", modulo: "A15", atendido: true },
+    { codigo: "S-225", nombre: "Alleria", tramite: "Reclamo de medicamentos", modulo: "B05", atendido: false },
+    { codigo: "S-184", nombre: "Sylvanas", tramite: "Autorización de cirugías", modulo: "C10", atendido: false },
+    { codigo: "S-092", nombre: "Veressa", tramite: "Gestión de servicios de urgencias", modulo: "D02", atendido: true },
+    { codigo: "S-103", nombre: "Arathor", tramite: "Inclusión de beneficiarios", modulo: "E09", atendido: true },
+    { codigo: "S-0124", nombre: "Nathanos", tramite: "Solicitud de historia clínica", modulo: "F13", atendido: false },
 ];
 
-const visornumero=document.getElementById("visornumero");
-const visorModulo=document.getElementById("visorModulo");
-const buscador=document.getElementById("buscador");
-const contadorFila=document.getElementById("contadorFila");
-const btnLlamar=document.getElementById("btnLlamar");
-const mensajeVacio=document.getElementById("mensajeVacio");
+const visornumero = document.getElementById("visornumero");
+const visorModulo = document.getElementById("visorModulo");
+const buscador = document.getElementById("buscador");
+const contadorFila = document.getElementById("contadorFila");
+const btnLlamar = document.getElementById("btnLlamar");
+const mensajeVacio = document.getElementById("mensajeVacio");
 
 function pintarfila() {
-    const listaEspera=document.getElementById("listaEspera");
-    turnos.length = 0;
+    const listaEspera = document.getElementById("listaEspera");
+    listaEspera.innerHTML = ""; /**innerHTML > elimina el contenido de ese "elemento" */
     for (let i = 0; i < turnos.length; i++) {
         const nuevoItem = document.createElement("li");
-        
+        listaEspera.appendChild(nuevoItem);
+        const nuevoSpan = document.createElement("span");
+        nuevoItem.appendChild(nuevoSpan);
+        const nuevoDiv = document.createElement("div");
+        nuevoItem.appendChild(nuevoDiv);
+        const nuevoP1 = document.createElement("p");
+        nuevoDiv.appendChild(nuevoP1);
+        const nuevoP2 = document.createElement("p");
+        nuevoDiv.appendChild(nuevoP2);
+        const otroSpan = document.createElement("span");
+        nuevoItem.appendChild(otroSpan);
+
+        nuevoItem.classList.add("turno"); /**classList.add para agregar class="turno" */
+        nuevoSpan.classList.add("turno_codigo");
+        nuevoDiv.classList.add("turno_datos");
+        nuevoP1.classList.add("turno_nombre");
+        nuevoP2.classList.add("turno_tramite");
+        otroSpan.classList.add("turno_estado")
     }
 }
